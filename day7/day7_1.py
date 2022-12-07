e9f8b1a81a0f8979560f3e587827a58db2dad79c
EdTheBass <68122094+EdTheBass@users.noreply.github.com>
2022-12-07 12:47:06 +0000

    Add files via upload

---
+++ day7/day7_1.py
@@ -0,0 +1,85 @@
+class Tree:
+    def __init__(self):
+        self.top = Node("/", "dir", 0)
+    
+    def dir_size(self, start=""):
+        if not start: start = self.top
+        total = 0
+        for x in start.children:
+            if x.n_type == "file":
+                total += x.size
+            else:
+                total += self.dir_size(start=x)
+        return total
+
+    def find_dir(self, dirname, start=""):
+        if not start: start = self.top
+        
+        if start.value == dirname and start.n_type == "dir":
+            return start
+        for x in start.children:
+            print(x.value, x.n_type)
+            if x.n_type == "file":
+                continue
+            
+            if x.value == dirname:
+                return x
+            else:
+                return self.find_dir(dirname, start=x)
+
+    def find_file(self, fname, start=""):
+        if not start: start = self.top
+        for x in start.children:
+            if x.n_type == "dir":
+                continue
+
+            if x.value == fname:
+                return x
+            else:
+                return find_file(fname, start=x)
+
+    def add_child(self, dirname, child):
+        directory = self.find_dir(dirname)
+        print(directory, dirname)
+        directory.children.append(child)
+
+class Node:
+    def __init__(self, value, n_type, size):
+        self.value = value
+        self.size = size 
+        self.n_type = n_type
+        self.children = []
+
+
+with open("day7.txt", "r") as f:
+    instructs = f.readlines()
+
+directory = Tree()
+print(directory.find_dir("/"))
+a_dir = Node("a", "dir", 0)
+e_dir = Node("e", "dir", 0)
+i_file = Node("i", "file", 584)
+f_file = Node("f", "file", 29116)
+g_file = Node("g", "file", 2557)
+h_file = Node("h.lst", "file", 62596)
+b_file = Node("b.txt", "file", 14848514)
+c_file = Node("c.dat", "file", 8504156)
+d_dir = Node("d", "dir", 0)
+j_file = Node("j", "file", 4060174)
+d_file = Node("d.log", "file", 8033020)
+d2_file = Node("d.ext", "file", 5626152)
+k_file = Node("k", "file", 7214296)
+
+directory.add_child("/", a_dir)
+directory.add_child("/", b_file)
+directory.add_child("/", c_file)
+directory.add_child("/", d_dir)
+directory.add_child("a", e_dir)
+directory.add_child("a", f_file)
+directory.add_child("a", g_file)
+directory.add_child("a", h_file)
+directory.add_child("d", j_file)
+directory.add_child("d", d_file)
+directory.add_child("d", d2_file)
+directory.add_child("d", k_file)
+
