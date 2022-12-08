d390ebfaa2bfa932af364c3abea1c0522aabc71e
EdTheBass <68122094+EdTheBass@users.noreply.github.com>
2022-12-08 10:14:19 +0000

    nearly there

--- day7/day7_1.py
+++ day7/day7_1.py
@@ -1,6 +1,6 @@
 class Tree:
     def __init__(self):
-        self.top = Node("/", "dir", 0)
+        self.top = Node("/", "dir", 0, None)
     
     def dir_size(self, start=""):
         if not start: start = self.top
@@ -41,46 +41,60 @@ class Tree:
 
     def add_child(self, dname, child):
         directory = self.find_dir(dname)
+        child.parent = directory
         directory.children.append(child)
 
 class Node:
-    def __init__(self, value, n_type, size):
+    def __init__(self, value, n_type, size, parent):
         self.value = value
         self.size = size 
         self.n_type = n_type
         self.children = []
+        self.parent = None
 
 
 with open("day7.txt", "r") as f:
     instructs = f.readlines()
 
-directory = Tree()
-a_dir = Node("a", "dir", 0)
-e_dir = Node("e", "dir", 0)
-i_file = Node("i", "file", 584)
-f_file = Node("f", "file", 29116)
-g_file = Node("g", "file", 2557)
-h_file = Node("h.lst", "file", 62596)
-b_file = Node("b.txt", "file", 14848514)
-c_file = Node("c.dat", "file", 8504156)
-d_dir = Node("d", "dir", 0)
-j_file = Node("j", "file", 4060174)
-d_file = Node("d.log", "file", 8033020)
-d2_file = Node("d.ext", "file", 5626152)
-k_file = Node("k", "file", 7214296)
+    top = Tree()
+    curr_dir = top
+    for _ in instructs:
+        i = _[:-1]
+        if i[0] == "$":
+            command = i[2:4]
+            if command == "cd":
+                directory = i[5:]
+                if directory == "/":
+                    curr_dir = top
+    
 
-directory.add_child("/", a_dir)
-directory.add_child("/", b_file)
-directory.add_child("/", c_file)
-directory.add_child("/", d_dir)
-directory.add_child("a", e_dir)
-directory.add_child("a", f_file)
-directory.add_child("a", g_file)
-directory.add_child("a", h_file)
-directory.add_child("e", i_file)
-directory.add_child("d", j_file)
-directory.add_child("d", d_file)
-directory.add_child("d", d2_file)
-directory.add_child("d", k_file)
+# directory = Tree()
+# a_dir = Node("a", "dir", 0)
+# e_dir = Node("e", "dir", 0)
+# i_file = Node("i", "file", 584)
+# f_file = Node("f", "file", 29116)
+# g_file = Node("g", "file", 2557)
+# h_file = Node("h.lst", "file", 62596)
+# b_file = Node("b.txt", "file", 14848514)
+# c_file = Node("c.dat", "file", 8504156)
+# d_dir = Node("d", "dir", 0)
+# j_file = Node("j", "file", 4060174)
+# d_file = Node("d.log", "file", 8033020)
+# d2_file = Node("d.ext", "file", 5626152)
+# k_file = Node("k", "file", 7214296)
 
-print(directory.dir_size())
+# directory.add_child("/", a_dir)
+# directory.add_child("/", b_file)
+# directory.add_child("/", c_file)
+# directory.add_child("/", d_dir)
+# directory.add_child("a", e_dir)
+# directory.add_child("a", f_file)
+# directory.add_child("a", g_file)
+# directory.add_child("a", h_file)
+# directory.add_child("e", i_file)
+# directory.add_child("d", j_file)
+# directory.add_child("d", d_file)
+# directory.add_child("d", d2_file)
+# directory.add_child("d", k_file) 
+
+# print(directory.dir_size())
