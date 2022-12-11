0d52cb5d8ea72f4340a237bff5f021a522ffb9b8
EdTheBass <68122094+EdTheBass@users.noreply.github.com>
2022-12-11 22:23:08 +0000

    day 9 was so easy compared to day 7 wtf

---
+++ day9/day9_2.py
@@ -0,0 +1,67 @@
+def parse_move(move):
+    return move[0], int(move[2:-1])
+
+def apply_move(pos, d):
+    x, y = pos
+    if d == "R":
+        return [x+1, y]
+    elif d == "L":
+        return [x-1, y]
+    elif d == "U":
+        return [x, y+1]
+    else:
+        return [x, y-1]
+
+def calc_tail_pos(head_pos, tail_pos):
+    hx, hy = head_pos
+    tx, ty = tail_pos
+    def adjacent():
+        dx = tx - hx
+        dy = ty - hy
+        if abs(dx) <= 1 and abs(dy) <= 1:
+            return True, dx, dy
+        return False, dx, dy
+
+    a, dx, dy = adjacent()
+    if a:
+        return tail_pos
+
+    s_dx = (dx//abs(dx)) if dx != 0 else 0
+    s_dy = (dy//abs(dy)) if dy != 0 else 0
+    
+    return [tx-s_dx, ty-s_dy]
+    
+
+with open("day9.txt", "r") as f:
+    moves = f.readlines()
+    
+    head_position = [0, 0]
+    k1_position = [0, 0]
+    k2_position = [0, 0]
+    k3_position = [0, 0]
+    k4_position = [0, 0]
+    k5_position = [0, 0]
+    k6_position = [0, 0]
+    k7_position = [0, 0]
+    k8_position = [0, 0]
+    tail_position = [0, 0]
+    visited = []
+    for _ in moves:
+        direction, magnitude = parse_move(_)
+        
+        for _ in range(magnitude):
+            head_position = apply_move(head_position, direction)
+            k1_position = calc_tail_pos(head_position, k1_position)
+            k2_position = calc_tail_pos(k1_position, k2_position)
+            k3_position = calc_tail_pos(k2_position, k3_position)
+            k4_position = calc_tail_pos(k3_position, k4_position)
+            k5_position = calc_tail_pos(k4_position, k5_position)
+            k6_position = calc_tail_pos(k5_position, k6_position)
+            k7_position = calc_tail_pos(k6_position, k7_position)
+            k8_position = calc_tail_pos(k7_position, k8_position)
+            tail_position = calc_tail_pos(k8_position, tail_position)
+            
+            if tail_position not in visited:
+                visited.append(tail_position)
+
+    print(len(visited))
